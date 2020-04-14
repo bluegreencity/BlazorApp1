fae055cc663671a7f19f76ef72d3c9c7f1245d38
Hyun-woo Shin <syndy123@naver.com>
2020-04-14 15:52:24 +0900

    20200414

---
+++ BlazorApp1/BlazorRocksBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1
+{
+    public class BlazorRocksBase : ComponentBase
+    {
+        public string BlazerRocksText { get; set; } = "Blazor rocks the browser!";
+    }
+}
---
+++ BlazorApp1/Pages/Counter_Partial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorApp1.Pages
+{
+    public partial class Counter_Partial
+    {
+        private int _currentCount = 0;
+
+        void IncrementCount()
+        {
+            _currentCount++;
+        }
+    }
+}
