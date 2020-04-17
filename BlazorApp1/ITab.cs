53b4a3f18819481ba85463c49dae8454a759d9db
Hyun-woo Shin <syndy123@naver.com>
2020-04-17 16:12:47 +0900

    2020.04.17

---
+++ BlazorApp1/ITab.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1
+{
+    public interface ITab
+    {
+        RenderFragment ChildContent { get; }
+    }
+}
